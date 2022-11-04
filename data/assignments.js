36653d1807800da857d3a0fb34129b44b13ceb60
planit <planit@tanjeffreyz.github.io>
2022-11-04 18:46:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -313,7 +313,7 @@ assignments = {
     {
       "title": "Homework 9 (Written)",
       "course": "CS 188",
-      "dueDate": "2022-11-16T22:59:00",
+      "dueDate": "2022-11-14T23:59:00",
       "link": "https://www.gradescope.com/courses/419636",
       "submitted": false
     }
