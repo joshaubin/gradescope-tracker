2c47fe28682cdee745f60880ce96b6e025633e67
planit <planit@tanjeffreyz.github.io>
2023-02-02 20:36:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "Homework 2 Coding Portion",
       "course": "CS 170",
       "dueDate": "2023-02-06T22:00:00",
-      "link": "https://www.gradescope.com/courses/496331",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/496331/assignments/2626945/submissions/158987401",
+      "submitted": true
     }
   ],
   "CS 184/284A": [
