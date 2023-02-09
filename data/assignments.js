ef821db559741d76fb19cc22bb279dd937eb9adb
planit <planit@tanjeffreyz.github.io>
2023-02-09 20:36:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,62 @@
 assignments = {
-  "CS 170": [],
-  "CS 184/284A": []
+  "CS 170": [
+    {
+      "title": "Homework 1 Coding Portion",
+      "course": "CS 170",
+      "dueDate": "2023-01-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/496331/assignments/2607222/submissions/157919533",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1 Written Portion",
+      "course": "CS 170",
+      "dueDate": "2023-01-30T22:00:00",
+      "link": "https://www.gradescope.com/courses/496331/assignments/2613868/submissions/158260907",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2 Coding Portion",
+      "course": "CS 170",
+      "dueDate": "2023-02-06T22:00:00",
+      "link": "https://www.gradescope.com/courses/496331/assignments/2626945/submissions/159380503",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2 Written Portion",
+      "course": "CS 170",
+      "dueDate": "2023-02-06T22:00:00",
+      "link": "https://www.gradescope.com/courses/496331/assignments/2637640/submissions/159990286",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3 Written Portion",
+      "course": "CS 170",
+      "dueDate": "2023-02-13T22:00:00",
+      "link": "https://www.gradescope.com/courses/496331",
+      "submitted": false
+    },
+    {
+      "title": "Homework 3 Coding Portion",
+      "course": "CS 170",
+      "dueDate": "2023-02-13T22:00:00",
+      "link": "https://www.gradescope.com/courses/496331",
+      "submitted": false
+    }
+  ],
+  "CS 184/284A": [
+    {
+      "title": "HW 1 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2596089/submissions/160217695",
+      "submitted": true
+    },
+    {
+      "title": "Project 1: Rasterizer",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    }
+  ]
 };
