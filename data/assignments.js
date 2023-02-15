0cee7b96dae04046926cb3d84656763f948969ee
planit <planit@tanjeffreyz.github.io>
2023-02-15 05:36:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -55,8 +55,8 @@ assignments = {
       "title": "Project 1: Rasterizer",
       "course": "CS 184/284A",
       "dueDate": "2023-02-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/483806/assignments/2627156/submissions/162047887",
+      "submitted": true
     },
     {
       "title": "Project 2 Checkpoint",
