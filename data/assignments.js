66fd824a06fb23f3543c610876ef000e572b8dbb
planit <planit@tanjeffreyz.github.io>
2023-04-18 18:35:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -152,7 +152,7 @@ assignments = {
     {
       "title": "Participation Tracker",
       "course": "CS 184/284A",
-      "dueDate": "2023-05-20T08:00:00",
+      "dueDate": "2023-05-31T08:00:00",
       "link": "https://www.gradescope.com/courses/483806/assignments/2665249/submissions/164030184",
       "submitted": true
     },
